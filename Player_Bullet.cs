141162ca23a537b718966ae3cb06ca177833fef5
DeadUser123 <kevinxia2015@gmail.com>
2024-11-08 19:42:31 -0500

    Added the ability for the player to shoot a bullet, removed duplication of test spawner

---
+++ Player_Bullet.cs
@@ -0,0 +1,33 @@
+using Godot;
+using System;
+
+public partial class Player_Bullet : CharacterBody2D
+{
+	public const float Speed = 300.0f;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 direction = Vector2.Up;
+		if (direction != Vector2.Zero) {
+			velocity.X = direction.X * Speed;
+			velocity.Y = direction.Y * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
--- TestSpawner.cs
+++ TestSpawner.cs
@@ -3,23 +3,30 @@ using System;
 
 public partial class TestSpawner : Node2D
 {
+	public static PackedScene Bullet { get; } = GD.Load<PackedScene>("res://TestProjectile.tscn");
+
+	private Node2D _player;
 	
-	public TestSpawner DuplicateSelf() {
-		TestSpawner duplicate = (TestSpawner)Duplicate();
-		return duplicate;
+	public void SpawnObject() {
+        if (Bullet == null || _player == null) return;
+
+        Node2D instance = (Node2D)Bullet.Instantiate();
+
+        instance.Position = _player.Position;
+
+        GetTree().CurrentScene.AddChild(instance);
 	}
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_player = GetNode<Node2D>("/root/Main/CharacterBody2D");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		if (Input.IsActionJustPressed("Test_Key")) {
-			var duplicateNode = DuplicateSelf();
-			duplicateNode.Position = new Vector2(200, 100);
-			GetParent().AddChild(duplicateNode);
+			SpawnObject();
 		}
 	}
 }
