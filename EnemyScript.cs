24ccb7798a1cfdf9665fb29f08cd9163d7cab3e8
DeadUser123 <kevinxia2015@gmail.com>
2025-01-03 13:10:41 -0500

    Fixed collisions, made bullets delete themselves
    ~ 1hr

--- EnemyScript.cs
+++ EnemyScript.cs
@@ -1,22 +1,29 @@
 using Godot;
 using System;
 
-public partial class EnemyScript : CharacterBody2D
-{
+public partial class EnemyScript : CharacterBody2D {
+	[Export] public float speed = 100f;        // Speed of the enemy
+    [Export] public float ShootingInterval = 2f; // Time between shots
+    private Vector2 _direction;               // Movement direction
+    private Timer _shootTimer;                // Timer for shooting
+	private Node2D _player;                   // Reference to the player
 	private Score scoreText; // Reference to the Score node
     public override void _Ready()
     {
-        Area2D area = GetNode<Area2D>("Area2D");
-		area.BodyEntered += OnBodyEntered;
+		
 		scoreText = GetNode<Score>("/root/Main/Score");
+
+		_player = GetNode<Node2D>("/root/Main/CharacterBody2D/CharacterBody2D");
+
+		// _direction = new Vector2(0, 1); // Move downward initially
+        // _shootTimer.Start();
     }
 
-	private void OnBodyEntered(Node body) {
-		GD.Print("Hit!");
+	public void GotHit() {
 		scoreText.ChangeScore(100);
 	}
-    public override void _Process(double delta)
-	{
-		
+    public override void _PhysicsProcess(double delta)
+    {
+		// Position += _direction * speed * delta;
 	}
 }
--- Player_Bullet.cs
+++ Player_Bullet.cs
@@ -3,35 +3,41 @@ using System;
 
 public partial class Player_Bullet : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    public const float Speed = 300.0f;
 
-	private void OnBodyEntered(Node body) {
-		GD.Print("oof!");
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		Vector2 velocity = Velocity;
+    }
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Vector2.Up;
-		if (direction != Vector2.Zero) {
-			velocity.X = direction.X * Speed;
-			velocity.Y = direction.Y * Speed;
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        Vector2 velocity = Velocity;
+
+        // Move the bullet upward
+        Vector2 direction = Vector2.Up;
+        if (direction != Vector2.Zero)
+        {
+            velocity.X = direction.X * Speed;
+            velocity.Y = direction.Y * Speed;
+        }
+        else
+        {
+            velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+            velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
+        }
+
+        KinematicCollision2D collision = MoveAndCollide(velocity * (float) delta);
+		if (collision != null) {
+			Node2D collider = collision.GetCollider() as Node2D;
+			if (collider.IsInGroup("Enemy")) {
+				var enemy = collider as EnemyScript;
+				enemy.GotHit();
+				QueueFree();
+			}
 		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
-		}
+    }
 
-		Velocity = velocity;
-		MoveAndSlide();
-	}
 }
