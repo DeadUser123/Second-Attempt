fb97cbe20c7d6c1b5d99ef3b7c6535dec11d341d
DeadUser123 <kevinxia2015@gmail.com>
2025-04-24 20:39:38 -0400

    added stuff for laser things

--- ToGameButton.cs
+++ ToGameButton.cs
@@ -3,16 +3,16 @@ using System;
 
 public partial class ToGameButton : Button
 {
-    // Called when the node enters the scene tree for the first time.
-    [Export] public string NextScenePath = "res://Gameplay.tscn";
+	// Called when the node enters the scene tree for the first time.
+	[Export] public string NextScenePath = "res://Gameplay.tscn";
 
-    public override void _Ready()
-    {
-        Pressed += OnButtonPressed;
-    }
+	public override void _Ready()
+	{
+		Pressed += OnButtonPressed;
+	}
 
-    private void OnButtonPressed()
-    {
-        GetTree().ChangeSceneToFile(NextScenePath);
-    }
+	private void OnButtonPressed()
+	{
+		GetTree().ChangeSceneToFile(NextScenePath);
+	}
 }
